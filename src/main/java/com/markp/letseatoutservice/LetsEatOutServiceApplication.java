c492007e070b05643a2982b67d81912ccd17af1d
Mark <markwp02@gmail.com>
2023-09-18 12:08:00 +1000

    Initial Commit
    Generated Project from Spring Boot Initializr

---
+++ src/main/java/com/markp/letseatoutservice/LetsEatOutServiceApplication.java
@@ -0,0 +1,13 @@
+package com.markp.letseatoutservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsEatOutServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsEatOutServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/markp/letseatoutservice/LetsEatOutServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markp.letseatoutservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsEatOutServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
